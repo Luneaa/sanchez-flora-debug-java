bebf296c0d028256f4c302e18a27848876caf33e
Luneaa <sanchez.flora11@gmail.com>
2023-10-23 16:28:56 +0200

    start refactoring analytics counter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,11 +3,17 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0;
 	private static int rashCount = 0;
 	private static int pupilCount = 0;
+
+	private final ISymptomReader reader;
+	private final ISymptomWriter writer;
 
 	public static void main(String args[]) throws Exception {
 		// first get input
@@ -36,4 +42,29 @@ public class AnalyticsCounter {
 		writer.write("dialated pupils: " + pupilCount + "\n");
 		writer.close();
 	}
+
+	public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
+
+		this.reader = reader;
+		this.writer = writer;
+	}
+
+	public List<String> getSymptoms() {
+		return this.reader.GetSymptoms();
+	}
+	public Map<String, Integer> countSymptoms(List<String> symptoms) {
+		HashMap<String, Integer> result = new HashMap<>();
+
+		// TODO: Implementer la methode
+
+		return result;
+	}
+	public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
+		// TODO: Implementer la methode
+		return null;
+	}
+	public void writeSymptoms(Map<String, Integer> symptoms) {
+		this.writer.writeSymptoms(symptoms);
+	}
+
 }
